d3ad65813a3191652a0466f3a3cbddbaf0918255
Stefan Marsiske <stefan.marsiske@gmail.com>
2010-10-25 02:18:19 +0200

    [enh] dream, tell and auth support added

--- magnus23.sh
+++ magnus23.sh
@@ -4,8 +4,38 @@ source ./configuration.sh
 
 mkdir -p "$EVENTS"
 
+function dream
+{
+   print "ACTION $(shuf $BASE_DIR/dreams.txt | head -1)"
+}
+
+function tell
+{
+   user="${1%%[ :]*}"
+   msg="${1#*[ :]}"
+   echo $user
+   echo $msg
+   echo "$user: $2 says \"$msg\"" >>"$BASE_DIR/tell/$user" &&
+      print "ACK" || print "NACK"
+}
+
+function dotell
+{
+   cat "$BASE_DIR/tell/$1" | while read line; do
+      print "$line" 
+   done && rm "$BASE_DIR/tell/$1" 
+}
+
+function auth
+{
+   grep -qs "^$1$" $BASE_DIR/auth || { 
+      print "NOOOOOOOOO!!11eleven!!!"
+      return 1
+   }
+}
 function post_event
 {
+  auth "$2" || return
   local text
   text="$(grep -q 8859 <<<"$(file - <<<"$1")" && \
           iconv -f latin1 -t utf-8 <<<"$1" || \
@@ -22,6 +52,7 @@ function post_event
 
 function post_tweet
 {
+  auth "$2" || return
   local text
   text="$(grep -q 8859 <<<"$(file - <<<"$1")" && \
           iconv -f latin1 -t utf-8 <<<"$1" || \
@@ -98,7 +129,7 @@ function translate_time
 
 function print_help
 {
-  print 'My available commands are: !tweet, !addquote, !lastquote, !randomquote, !addevent, !listevents, !delevent, !postevent'
+  print 'My available commands are: !tweet, !addquote, !lastquote, !randomquote, !addevent, !listevents, !delevent, !postevent, !tell'
 }
 
 function addquote
@@ -155,31 +186,37 @@ function handle_commands
   tail --pid=$$ -fn0 "$IRC_CONNECTIONS/$IRC_HOST/$IRC_CHAN/out" | while read MSG
   do
     message_text="$(msg_text)"
+
+    [[ -f "$BASE_DIR/tell/$(msg_nick)" ]] && dotell "$(msg_nick)"
+
+    case "$message_text" in
+       !help)
+          print_help ;;
+       !addquote\ *)
+          addquote "${message_text#\!addquote }" ;;
+       !lastquote)
+          print "Last quote: $(tail -n1 "$QUOTES")" ;;
+       !quote|!randomquote)
+          print "$(sed -n $[ 1 + $RANDOM % $(grep -c '' "$QUOTES") ]p "$QUOTES")" ;;
+       !addevent\ *)
+          addevent "${message_text#\!addevent }" ;;
+       !delevent\ *)
+          delevent "$(cut -d' ' -f2 <<<"$message_text")" ;;
+       !listevents)
+          listevents ;;
+       !postevent\ *)
+          post_event "${message_text#\!postevent }" "$(msg_nick)" ;;
+       !tweet\ *)
+          post_tweet "${message_text#\!tweet }" "$(msg_nick)" ;;
+       !twitter\ *)
+          post_tweet "${message_text#\!twitter }" "$(msg_nick)" ;;
+       !tell\ *)
+          tell "${message_text#\!tell }" "$(msg_nick)" ;;
+    esac 
+
     case "$message_text" in
       *${IRC_NICK}*)
-        print "ACTION alszik" ;;
-      !help)
-        print_help ;;
-      !addquote\ *)
-        addquote "${message_text#\!addquote }" ;;
-      !lastquote)
-        print "Last quote: $(tail -n1 "$QUOTES")" ;;
-      !quote|!randomquote)
-        print "$(sed -n $[ 1 + $RANDOM % $(grep -c '' "$QUOTES") ]p "$QUOTES")" ;;
-      !addevent\ *)
-        addevent "${message_text#\!addevent }" ;;
-      !delevent\ *)
-        delevent "$(cut -d' ' -f2 <<<"$message_text")" ;;
-      !listevents)
-        listevents ;;
-      !postevent\ *)
-        post_event "${message_text#\!postevent }" "$(msg_nick)" ;;
-      !tweet\ *)
-        post_tweet "${message_text#\!tweet }" "$(msg_nick)" ;;
-      !twitter\ *)
-        post_tweet "${message_text#\!twitter }" "$(msg_nick)" ;;
-      #!tell\ *)
-      #  addevent "${message_text#\!tell }" ;;
+         dream ;;
     esac
   done
 }
